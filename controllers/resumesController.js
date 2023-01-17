731654102f5c302b4c0f570c9327c65dc1558bfc
Ramachandran_Rajeevaletshanth <vjvfc2k16@gmail.com>
2023-01-17 08:55:47 +0530

    reforw 1

--- controllers/resumesController.js
+++ controllers/resumesController.js
@@ -1,20 +1,8 @@
 require("dotenv").config();
 const logger = require("../config/logger");
 const Resumes = require("../models/resumes");
-const Upload = require('../models/upload');
 const transporter = require("../services/nodemailer/mailer");
 
-const multer = require('multer');
-const path = require('path');
-
-
-const storage = multer.diskStorage({
-    destination: path.join(__dirname, '../public', 'resumes'),
-    filename: function (req, file, cb) {   
-        // null as first argument means no error
-        cb(null, Date.now() + '-' + file.originalname.replace(/\s/g, ''));  
-    }
-});
 
 const upload = multer({ storage: storage });
 
@@ -24,45 +12,20 @@ module.exports = {
     const name = req.body.name;
     const email = req.body.email;
     const phone = req.body.phone;
-    // const file = req.body.resume;
+    const resume = req.body.resume;
 
     try {
-        let upload = multer({ storage: storage}).single('file');
-
-        upload(req, res, async (err) => {
-
-            if (!req.file) {
-                return res.send('Please select a file to upload');
-            }
-            else if (err instanceof multer.MulterError) {
-                return res.send(err);
-            }
-            else if (err) {
-                return res.send(err);
-            }
-
-            const classifiedsadd = {
-                file: req.file.filename
-            };
-
-
-            const upload = new Upload(classifiedsadd)
-            await upload.save()
-            
             const newResume = new Resumes({
                 name: name,
                 email: email,
                 phone: phone,
-                resume: upload.file
+                resume: resume
             })
             await newResume.save();
             if(newResume)
                 res.json({ response: "success", message: "Your cv uploaded successfully." })
             else
                 res.json({ response: "error", message: "Sorry cannot upload file. Please try again later." })
-
-        }); 
-
     } catch (error) {
         return res.json({response: "error", message: error.message});
     }
